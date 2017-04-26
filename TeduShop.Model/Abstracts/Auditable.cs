8176d587341cb549a4265ad61653835ed9832209
DESKTOP-CLD0TCS\Do Xuan Trung <bstrungk40@gmail.com>
2017-04-26 15:02:22 +0700

    Use for practice

---
+++ TeduShop.Model/Abstracts/Auditable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstracts
+{
+    public abstract class Auditable:IAuditable
+    {
+        [MaxLength(256)]
+        public string MetaKeyword { set; get; }
+        [MaxLength(256)]
+        public string MetaDescription { set; get; }
+
+        public DateTime? CreatedDate { set; get; }
+        [MaxLength(256)]
+        public string CreatedBy { set; get; }
+
+        public DateTime? UpdatedDate { set; get; }
+
+        [MaxLength(256)]
+        public string UpdateBy { set; get; }
+
+        public bool Status { set; get; }
+        DateTime IAuditable.CreatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        DateTime IAuditable.UpdatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ TeduShop.Model/Abstracts/IAuditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstracts
+{
+    public interface IAuditable
+    {
+         string MetaKeyword { set; get; }
+         string MetaDescription { set; get; }
+         DateTime CreatedDate { set; get; }
+         string CreatedBy { set; get; }
+         DateTime UpdatedDate { set; get; }
+         string UpdateBy { set; get; }
+         bool Status { set; get; }
+    }
+}
