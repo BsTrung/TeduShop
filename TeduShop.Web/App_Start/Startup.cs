56ae2e558c65c8ac19ec80e8e1fa58c6e3b61156
DESKTOP-CLD0TCS\Do Xuan Trung <bstrungk40@gmail.com>
2017-05-01 17:51:34 +0700

    lesson 13

---
+++ TeduShop.Web/App_Start/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TeduShop.Web.App_Start.Startup))]
+
+namespace TeduShop.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigAutofac(app);
+        }
+        private void ConfigAutofac(IAppBuilder ap)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<>
+        }
+    }
+}
