b37660ae66aaaf01d420079573464d1d6684dbeb
DESKTOP-CLD0TCS\Do Xuan Trung <bstrungk40@gmail.com>
2017-05-01 18:48:29 +0700

    finish lesson 13

--- TeduShop.Web/App_Start/Startup.cs
+++ TeduShop.Web/App_Start/Startup.cs
@@ -1,7 +1,15 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
 using Microsoft.Owin;
 using Owin;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
+using TeduShop.Data;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Service;
 
 [assembly: OwinStartup(typeof(TeduShop.Web.App_Start.Startup))]
 
@@ -14,12 +22,33 @@ namespace TeduShop.Web.App_Start
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
             ConfigAutofac(app);
         }
-        private void ConfigAutofac(IAppBuilder ap)
+
+        private void ConfigAutofac(IAppBuilder app)
         {
             var builder = new ContainerBuilder();
             builder.RegisterControllers(Assembly.GetExecutingAssembly());
 
-            builder.RegisterType<>
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+            builder.RegisterType<TeduShopDbContext>().AsSelf().InstancePerRequest();
+
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(PostCategoryRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Service
+            builder.RegisterAssemblyTypes(typeof(PostCategoryService).Assembly)
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Container
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container);
         }
     }
 }
